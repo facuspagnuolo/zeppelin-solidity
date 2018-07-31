9bffca97df2231d73d538c15e762395af3b57276
Alan Lu <alanlu1023@gmail.com>
2018-07-31 17:51:21 -0500

    Fixed comment for safe sdiv

--- contracts/math/SafeMath.sol
+++ contracts/math/SafeMath.sol
@@ -52,7 +52,7 @@ library SafeMath {
   * @dev Integer division of two signed integers, truncating the quotient.
   */
   function div(int256 a, int256 b) internal pure returns (int256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    // assert(b != 0); // Solidity automatically throws when dividing by 0
     // Overflow only happens when the smallest negative int is multiplied by -1.
     assert(a != INT256_MIN || b != -1);
     return a / b;
